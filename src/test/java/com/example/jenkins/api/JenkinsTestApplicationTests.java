7b5a82942621d747f3b63d12b6d354242abb379f
e <ramxyz5595@gmail.com>
2025-02-21 17:48:02 +0530

    second commit

--- src/test/java/com/example/jenkins/api/JenkinsTestApplicationTests.java
+++ src/test/java/com/example/jenkins/api/JenkinsTestApplicationTests.java
@@ -17,6 +17,7 @@ class JenkinsTestApplicationTests {
 	public void contextLoads() {
 
 		logger.info("Test case executing...");
+		logger.info("Test case executing second log statement...");
 		assertEquals(true, true);
 	}
 
